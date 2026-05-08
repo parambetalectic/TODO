661d4ef01102afb9bbbf8ce91845a290c6f710cb
parambetalectic <paramveer@betalectic.com>
2026-05-08 14:42:15 +0530

    chore: setup backend app

---
+++ backend/src/config/db.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export const connectDB = async () => {
+  await pool.connect();
+  console.log(" DB connected");
+};
